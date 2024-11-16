73ad5c89dc1468bf326471b68942fc53d86ce41a
Shuhei Kuno <circular.nine@gmail.com>
2024-11-16 21:06:39 +0900

    implemented body tracking with MediaPipe and then send these data to VRChat

--- README.md
+++ README.md
@@ -9,9 +9,21 @@
 
 使用ライブラリ：
  - [python-osc](https://pypi.org/project/python-osc/)
+   - VRChatへのトラッキングデータの送信のため
 
 ## カメラトラッキング
 
-使用ライブラリ
+使用ライブラリ：
  - [opencv-python](https://pypi.org/project/opencv-python/)
+   - カメラから動画を取得するため
  - [MediaPipe](https://ai.google.dev/edge/mediapipe/solutions/guide?hl=ja)
+   - 画像から姿勢を推測するため
+ - [NumPy](https://numpy.org/)
+   - 計算用
+
+## コントロール
+
+使用ライブラリ：
+ - [Flask](https://flask.palletsprojects.com/en/stable/)
+   - 外部からプログラムをコントロールするためのインタフェースを作成するため
+ 
--- xvr/osc_client_test.py
+++ xvr/osc_client_test.py
@@ -6,28 +6,28 @@ from pythonosc import udp_client
 def main():
     client = udp_client.SimpleUDPClient("127.0.0.1", 9000)
 
-    for x in range(1000):
-        client.send_message(f"/tracking/trackers/1/position", [-0.125, 0.0, 0.0]) # right feet
-        client.send_message(f"/tracking/trackers/1/rotation", [0.0, 0.0, 0.0])
-        client.send_message(f"/tracking/trackers/2/position", [0.125, 0.0, 0.0]) # left feet
-        client.send_message(f"/tracking/trackers/2/rotation", [0.0, 0.0, 0.0])
+    for x in range(10000000):
+        client.send_message(f"/tracking/trackers/1/position", [-0.125, 0.2, 0.0]) # right feet
+        client.send_message(f"/tracking/trackers/1/rotation", [0.0, 0.0, x / 100])
+        client.send_message(f"/tracking/trackers/2/position", [0.125, 0.2, 0.0]) # left feet
+        # client.send_message(f"/tracking/trackers/2/rotation", [0.0, 0.0, 0.0])
 
         client.send_message(f"/tracking/trackers/3/position", [-0.125, 0.55, 0.0]) # right knees
-        client.send_message(f"/tracking/trackers/3/rotation", [0.0, 0.0, 0.0])
+        # client.send_message(f"/tracking/trackers/3/rotation", [0.0, 0.0, 0.0])
         client.send_message(f"/tracking/trackers/4/position", [0.125, 0.55, 0.0]) # left knees
-        client.send_message(f"/tracking/trackers/4/rotation", [0.0, 0.0, 0.0])
+        # client.send_message(f"/tracking/trackers/4/rotation", [0.0, 0.0, 0.0])
 
         client.send_message(f"/tracking/trackers/5/position", [0.0, 0.85, 0.0]) # hip
-        client.send_message(f"/tracking/trackers/5/rotation", [0.0, 0.0, 0.0])
+        # client.send_message(f"/tracking/trackers/5/rotation", [0.0, 0.0, 0.0])
         client.send_message(f"/tracking/trackers/6/position", [0.0, 1.35, 0.0]) # chest
-        client.send_message(f"/tracking/trackers/6/rotation", [0.0, 0.0, 0.0])
+        # client.send_message(f"/tracking/trackers/6/rotation", [0.0, 0.0, 0.0])
 
         client.send_message(f"/tracking/trackers/7/position", [-0.4, 1.5, 0.0]) # right elbows
-        client.send_message(f"/tracking/trackers/7/rotation", [0.0, 0.0, 0.0])
+        # client.send_message(f"/tracking/trackers/7/rotation", [0.0, 0.0, 0.0])
         client.send_message(f"/tracking/trackers/8/position", [0.4, 1.5, 0.0]) # left elbows
-        client.send_message(f"/tracking/trackers/8/rotation", [0.0, 0.0, 0.0])
+        # client.send_message(f"/tracking/trackers/8/rotation", [0.0, 0.0, 0.0])
 
-        time.sleep(1)
+        time.sleep(0.05)
         print("hoge")
 
 if __name__ == "__main__":
